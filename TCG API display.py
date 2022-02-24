91312fe1b1440caa7f0be84ec44e375304cfd4ac
rainddjohn <85827646+rainddjohn@users.noreply.github.com>
2022-02-23 22:55:18 -0500

    Update TCG API display.py

--- TCG API display.py
+++ TCG API display.py
@@ -1,7 +1,10 @@
+#text file you want to display
+openfile='crimson_vow'
+
 import pygame;pygame.init();set='Crimson Vow'
 (width,height)=(300,900);background_color= (0,0,0)
 screen=pygame.display.set_mode((width,height));screen.fill(background_color);pygame.display.set_caption(f'{set}')
-f= open('crimson_vow.txt', 'r');list1=[];list2=[]
+f= open(f'{openfile}.txt', 'r');list1=[];list2=[]
 #takes txt file and converts it into a list of strings
 for i in f:
     if i=='\n':continue
